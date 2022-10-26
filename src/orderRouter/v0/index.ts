b3ea93429bde0e4bba9e6b52a64b65b2e9933910
Phil Gabardo <gabardpj@gmail.com>
2022-10-26 15:07:02 -0700

    Add exports for DexQuote classes

--- src/orderRouter/v0/index.ts
+++ src/orderRouter/v0/index.ts
@@ -1,7 +1,19 @@
 // imports
 import DeflexOrderRouterClient from "./DeflexOrderRouterClient"
+import DeflexPathElement from "./DeflexPathElement"
+import DeflexQuote from "./DeflexQuote"
+import DeflexRoute from "./DeflexRoute"
+import DeflexTransaction from "./DeflexTransaction"
+import DeflexTransactionGroup from "./DeflexTransactionGroup"
+import DexQuote from "./DexQuote"
 
 // exports
 export {
-	DeflexOrderRouterClient
+	DeflexOrderRouterClient,
+	DeflexPathElement,
+	DeflexQuote,
+	DeflexRoute,
+	DeflexTransaction,
+	DeflexTransactionGroup,
+	DexQuote
 }
